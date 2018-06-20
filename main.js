e1bb52e57567f281af0b74f7f1b84d24df596fab
Marllon Panisset <marllon.panisset@sirius.com.br>
2018-06-20 10:16:19 -0300

    Versão 2.0

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-var form = document.getElementById("form");
 var nome = document.getElementById("nome");
 var email = document.getElementById("email");
 var assunto = document.getElementById("assunto");
 var mensagem = document.getElementById("mensagem");
-var enviar = document.getElementById("enviar");
-var display = document.getElementById("display");
 var displayNome = document.getElementById("displayNome");
 var displayEmail = document.getElementById("displayEmail");
 var displayAssunto = document.getElementById("displayAssunto");
 var displayMensagem = document.getElementById("displayMensagem");
 
 function getData() {
-    if (nome.value.length <=0) {
-        alert("Nome não pode ser vazio!");
+    if (nome.value.length <= 0) {
+        nome.nextElementSibling.classList.add("show");
+    } else{
+        nome.nextElementSibling.classList.remove("show");
     }
     if (email.value.length <= 0) {
-        alert("Email não pode ser vazio!");
+        email.nextElementSibling.classList.add("show");
+    } else {
+        email.nextElementSibling.classList.remove("show");
     }
     if (assunto.value.length <= 0) {
-        alert("Assunto não pode ser vazio!");
+        assunto.nextElementSibling.classList.add("show");
+    } else {
+        assunto.nextElementSibling.classList.remove("show");
     }
     if (mensagem.value.length <= 0) {
-        alert("Mensagem não pode ser vazio!");
+        mensagem.nextElementSibling.classList.add("show");
+    } else {
+        mensagem.nextElementSibling.classList.remove("show");
     }
-    else if (nome.value.length > 0 && email.value.length > 0 && assunto.value.length > 0 && mensagem.value.length > 0){
+    if (nome.value.length > 0 && email.value.length > 0 && assunto.value.length > 0 && mensagem.value.length > 0){
         displayNome.innerHTML += nome.value;
         displayEmail.innerHTML += email.value;
         displayAssunto.innerHTML += assunto.value;
@@ -31,13 +36,9 @@ function getData() {
     }
 }
 
-// var limpar = document.getElementById("limpar").onclick = function() {
-//     limparDisplay()
-// };
-
 function limparDisplay() {
-    displayNome.value = "";
-    displayEmail.value = "";
-    displayAssunto.value = "";
-    displayMensagem.value = "";
+    displayNome.innerHTML = "";
+    displayEmail.innerHTML = "";
+    displayAssunto.innerHTML = "";
+    displayMensagem.innerHTML = "";
 }
